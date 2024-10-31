976d0a23791d94f47997c4857987092ce5c16f55
brain188 <tendongnkengafac@gmail.com>
2024-10-31 17:25:58 +0100

    first commit

---
+++ App.py
@@ -0,0 +1,14 @@
+# App.py
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return jsonify(message="Hello level 400 FET, Quality Assurance!")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
